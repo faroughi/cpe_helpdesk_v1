3bd2730a5b4f0eb4e3abc346cce571d749bc88a9
aravind karnam <aravind.karanam@gmail.com>
2017-05-18 20:39:01 -0700

    check if the path is working on heroku like it does on azure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var courseStore = fs.readFileSync(process.cwd()+'/courses.json','utf8');
+console.log(courseStore);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/yes',function(req,res,next){
+  res.send(JSON.stringify(courseStore));
+})
 
 module.exports = router;
